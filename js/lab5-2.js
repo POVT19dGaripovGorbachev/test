defbe958f6da3564dc4d571138129b3cb4f803ab
unknown <mihail.garipov36@gmail.com>
2021-10-20 16:57:39 +0500

    comit

---
+++ js/lab5-2.js
@@ -0,0 +1,10 @@
+function showMessage() {
+    alert( 'ааааааа' );
+  }
+  function func1() {
+	alert(1);
+}
+
+function func2() {
+	alert(2);
+}
--- js/lab5.js
+++ js/lab5.js
@@ -144,5 +144,158 @@ console.log(arr);
 arr= [3, 4, 1, 2, 7];
 arr.sort();
 console.log(arr);
+//34
+
+arr = {
+	"ru":['голубой', 'красный', 'зеленый'],
+	'en':['blue', 'red', 'green'],
+};
+console.log(arr.ru[0]);
+//35
+
+user = {
+	name: "name",
+  surname:"surname",
+  patronymic:"patronymic"
+};
+console.log(user.name+" "+user.surname+" "+user.patronymic);
+//37
+arr=[ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+console.log(arr[1][0]);
+//38
+obj={js:['jQuery', 'Angular'], php: 'hello', css: 'world'};
+console.log(obj.js[0]);
+//39
+
+obj={ru:['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'], 
+eng: ['Monday', 'Tuesday ', 'Wednesday ', 'Thursday', 'Friday ', 'Saturday', 'Sunday'],
+
+};
 
 
+console.log(obj.ru[0]+'  '+obj.eng[2]);
+//40
+lang=`eng`;
+day=5;
+console.log(obj[lang][day]);
+//41
+obj={js:'test', jq: 'hello', css: 'world'};
+console.log(Object.keys(obj));
+//42
+test1=20;
+test2=31;
+if (test1>test2){
+console.log(test1)
+
+}
+else{
+console.log(test2)
+
+}
+//43
+
+num=3;
+if ((num>0)&&(num<5)){
+console.log("verno")
+
+}
+else{
+console.log("neverno")
+
+}
+//44
+
+num1=3;
+num2=4;
+if ((num1>=1)&&(num2<=3)){
+console.log("verno")
+
+}
+else{
+console.log("neverno")
+
+}
+//45
+num=83;
+if ((num>=1)&&(num<=3)) 
+console.log("ne popadaet")
+
+
+else
+if (num/10+num%10<=9)
+
+console.log("summa cifr odnoznachna")
+else
+  console.log("summa cifr dvuznachna")
+
+
+//46
+let age = 137; let adult;
+if (age >= 18) {
+	 adult = true;
+} else {
+	 adult = false;
+}
+console.log(adult);
+//47
+num=2390;
+if (num%10==0)
+console.log("true")
+else console.log("false");
+//48
+ month = 4;
+
+if (month == 12 || month >= 1 && month <= 2){
+console.log('зима');
+}
+if (month >2 && month <= 5){
+console.log ('весна');
+}
+if (month > 5 && month <= 8){
+console.log ('лето');
+}
+if (month > 8 && month <= 11){
+console.log ('осень');
+}
+
+//49
+num=1;
+switch(num) {
+  case 1:console.log("zima") ; // if (x === 'value1')
+  break;
+   
+
+  case 2:console.log("vesna");  // if (x === 'value1')
+  break;
+  case 3:console.log("leto") ; // if (x === 'value1')
+  break;
+  case 4:console.log("osen") ; // if (x === 'value1')
+  break;
+default:console.log("oshibka");
+  
+} 
+//50
+for (i=1;i<=100;i++) if (i%2==0)console.log(i);
+//51
+for (i=30;i>=0;i--) console.log(i);
+//52
+num=1;
+for (i=1;i<=20;i++) num*=i;console.log(num);
+//53
+num=1;
+for (i=2;i<=100;i++) if (i%2==0) num+=i; console.log(num);
+//54
+arr=[1,2,3,4,5];
+for (i=0;i<arr.length;i++) if (arr[i]%2==1) console.log(arr[i]);
+//55
+num=0;
+arr=[1,2,3,4,5];
+for (i=0;i<arr.length;i++) num+=arr[i]*arr[i]; console.log(num);
+//56
+let date = new Date(); date= date.getDay();
+arr=['воскресенье','понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', ]
+for (i=0;i<arr.length;i++) if (arr[i]==arr[date]) console.log(arr[date].toUpperCase())
+ else console.log(arr[i]);
+
+ 
+ 
